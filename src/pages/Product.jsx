016d7c560606c77abc259ea964ec3132529c3a41
YudProj <saodksad12@gmail.com>
2025-06-30 19:21:27 +0700

    hapus product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ProductList from "../components/ProductList";
+// import React from "react";
+// import ProductList from "../components/ProductList";
 
-export default function Products() {
-  return (
-    <div>
-      <ProductList />
-    </div>
-  );
-}
+// export default function Products() {
+//   return (
+//     <div>
+//       <ProductList />
+//     </div>
+//   );
+// }
